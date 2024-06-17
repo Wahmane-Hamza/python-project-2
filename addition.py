839c5a5f2f5b50830424ebe722e2fb9f72be4b59
hamza wahmane <hamza.wahm5@gmail.com>
2024-06-17 06:06:58 +0100

    add

---
+++ addition.py
@@ -0,0 +1,6 @@
+firstNumber = int(input('type the first number: '))
+secundNumber = int(input('type the secund number: '))
+some=0
+for i in range(secundNumber):
+    some=some+firstNumber
+print(some)
