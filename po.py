94481ca440dfd1cebcf9d18798c7f047926af6f7
hamza wahmane <hamza.wahm5@gmail.com>
2024-06-17 06:08:06 +0100

    po

---
+++ po.py
@@ -0,0 +1,8 @@
+nom1 = float(input('ecrire le premier nomber'))
+nom2 = float(input('ecrire le deuxieme nomber'))
+if nom1>0 and nom2>0 :
+    print('la produit est positif')
+elif nom1<0 and nom2<0:
+    print('la produit est positivf')    
+else:
+    print('la produit est nigatif')
